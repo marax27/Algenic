df4618cbd244980273eb5019077f2db4533f1b3d
marax27 <kacpert314@gmail.com>
2019-11-14 20:13:24 +0100

    Add example unit test

--- Algenic.UnitTests/SampleUnitTests.cs
+++ Algenic.UnitTests/SampleUnitTests.cs
@@ -6,6 +6,16 @@ namespace Algenic.UnitTests
 {
     public class SampleUnitTests
     {
-        
+        [Fact]
+        public void ExampleTest()
+        {
+            Func<int, int> action = x => x * x;
+            var givenValue = 42;
+            var expectedValue = givenValue * givenValue;
+
+            var actualValue = action(givenValue);
+
+            actualValue.Should().Be(expectedValue);
+        }
     }
 }

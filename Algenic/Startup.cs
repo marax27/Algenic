127ab41787562f876f747be46cfe324f37e13b90
marax27 <kacpert314@gmail.com>
2019-11-15 20:38:45 +0100

    Do not enforce HTTPS in dev environment

--- Algenic/Startup.cs
+++ Algenic/Startup.cs
@@ -56,12 +56,12 @@ namespace Algenic
             }
             else
             {
+                app.UseHttpsRedirection();
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 

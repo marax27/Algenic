9902ff027a90296057f58e2142510ced766dd953
marax27 <kacpert314@gmail.com>
2019-11-25 21:15:37 +0100

    Add default roles to a database

--- Algenic/Data/ApplicationDbContext.cs
+++ Algenic/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Algenic.Data.Initializers;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Algenic.Data
@@ -8,6 +9,13 @@ namespace Algenic.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyDefaultRoles();
         }
     }
 }

26a7e5131a60455e892e8329760a7459611ac96b
SlawekKalandyk <slawek.kalandyk@gmail.com>
2019-11-28 19:42:56 +0100

    Added DbSets of models to ApplicationDbContext

--- Algenic/Data/ApplicationDbContext.cs
+++ Algenic/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Algenic.Data.Initializers;
+using Algenic.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,14 @@ namespace Algenic.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Solution> Solutions { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<CompilationResult> CompilationResults { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Contest> Contests { get; set; }
+        public DbSet<ScorePolicy> ScorePolicies { get; set; }
+        public DbSet<ScoreRule> ScoreRules { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
@@ -16,6 +25,13 @@ namespace Algenic.Data
             base.OnModelCreating(builder);
 
             builder.ApplyDefaultRoles();
+            builder.Entity<Solution>().ToTable(nameof(Solution));
+            builder.Entity<Task>().ToTable(nameof(Task));
+            builder.Entity<CompilationResult>().ToTable(nameof(CompilationResult));
+            builder.Entity<Test>().ToTable(nameof(Test));
+            builder.Entity<Contest>().ToTable(nameof(Contest));
+            builder.Entity<ScorePolicy>().ToTable(nameof(ScorePolicy));
+            builder.Entity<ScoreRule>().ToTable(nameof(ScoreRule));
         }
     }
 }

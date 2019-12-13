03ef86997d87b1a4b39e9b70f8b22cbd96b5f644
SlawekKalandyk <slawek.kalandyk@gmail.com>
2019-12-13 13:28:41 +0100

    Add redirections to Login and AccessDenied pages

---
+++ Algenic/Routing/DefaultRedirections.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Algenic.Routing
+{
+    public class DefaultRedirections
+    {
+        private PageModel PageModel { get; set; }
+        public DefaultRedirections(PageModel pageModel)
+        {
+            PageModel = pageModel;
+        }
+
+        public IActionResult ToAccessDeniedPage(string returnUrl)
+        {
+            return PageModel.RedirectToPage("/Account/AccessDenied", new { area = "Identity", ReturnUrl = returnUrl });
+        }
+
+        public IActionResult ToLoginPage(string returnUrl)
+        {
+            return PageModel.RedirectToPage("/Account/Login", new { area = "Identity", ReturnUrl = returnUrl });
+        }
+    }
+}

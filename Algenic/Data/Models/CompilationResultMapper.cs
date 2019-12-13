ef635deced4b44b3859a0288a3a87d807d86667e
matski98 <34057263+matski98@users.noreply.github.com>
2019-12-13 17:50:19 +0100

    Add Mapper from JDoodleOutput to CompilationResult

---
+++ Algenic/Data/Models/CompilationResultMapper.cs
@@ -0,0 +1,17 @@
+﻿using Algenic.Compilation.Outputs;
+namespace Algenic.Data.Models
+{
+    public class CompilationResultMapper
+    {
+        public CompilationResult Map(JDoodleOutput output)
+        {
+            CompilationResult result = new CompilationResult();
+            result.CpuTime = output.CpuTime;
+            result.MemoryUsage = output.Memory;
+            if (output.StatusCode == "200") result.ExecutionSuccessful = true;
+            else result.ExecutionSuccessful = false;
+            result.Output = output.Output;
+            return result;
+        }
+    }
+}

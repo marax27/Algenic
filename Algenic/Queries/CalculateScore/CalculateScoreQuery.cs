9451b35a229b6b4c1d4279d9aa6f1fd0b8121b60
marax27 <kacpert314@gmail.com>
2019-12-15 21:38:56 +0100

    Add CalculateScore query

---
+++ Algenic/Queries/CalculateScore/CalculateScoreQuery.cs
@@ -0,0 +1,19 @@
+﻿using Algenic.Commons.DesignByContract;
+
+namespace Algenic.Queries.CalculateScore
+{
+    public class CalculateScoreQuery
+    {
+        public int SolutionId { get; }
+
+        private CalculateScoreQuery(int solutionId)
+        {
+            SolutionId = solutionId;
+        }
+
+        public static CalculateScoreQuery Create(int solutionId)
+        {
+            return new CalculateScoreQuery(solutionId);
+        }
+    }
+}
---
+++ Algenic/Queries/CalculateScore/CalculateScoreQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Algenic.Commons;
+using Algenic.Data;
+using Algenic.Data.Models;
+
+namespace Algenic.Queries.CalculateScore
+{
+    public class CalculateScoreQueryHandler : IQueryHandler<CalculateScoreQuery, CalculateScoreResult>
+    {
+        private ApplicationDbContext _dbContext;
+
+        public CalculateScoreQueryHandler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<CalculateScoreResult> HandleAsync(CalculateScoreQuery query)
+        {
+            var solution = await _dbContext.Solutions.FindAsync(query.SolutionId);
+            var percentagePassed = await GetPassedTestPercentage(solution);
+
+            var rules = solution.Task.ScorePolicy.ScoreRules
+                .OrderBy(sr => sr.Threshold)
+                .ToList();
+
+            int score = 0;
+            for (int i = 0; i != rules.Count - 1; ++i)
+            {
+                var rule = rules[i];
+                if (percentagePassed < rule.Threshold)
+                    break;
+                score = rule.Score;
+            }
+
+            return new CalculateScoreResult(score);
+        }
+
+        private async Task<decimal> GetPassedTestPercentage(Solution solution)
+        {
+            var passedTestCount = solution.CompilationResults
+                .Count(cr => cr.ExecutionSuccessful && cr.Output == cr.Test.ExpectedOutput);
+
+            var allTestCount = solution.Task.Tests.Count;
+
+            return new decimal(passedTestCount) / new decimal(allTestCount);
+        }
+    }
+}

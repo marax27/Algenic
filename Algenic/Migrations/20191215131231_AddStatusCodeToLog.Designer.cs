6ba9ac73cf12a635d22d950214d3c8c52ab5f4f4
SlawekKalandyk <slawek.kalandyk@gmail.com>
2019-12-15 14:13:18 +0100

    Add migration creating StatusCode column in Logs table

--- Algenic/Migrations/20191215131231_AddStatusCodeToLog.Designer.cs
+++ Algenic/Migrations/20191215131231_AddStatusCodeToLog.Designer.cs
@@ -4,14 +4,16 @@ using Algenic.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Algenic.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191215131231_AddStatusCodeToLog")]
+    partial class AddStatusCodeToLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

4027672b7bcf6d683e0eb2c9ac3a27caf6f7b7b8
marax27 <kacpert314@gmail.com>
2019-12-15 16:27:53 +0100

    Add VerifySolutionCommand

---
+++ Algenic/Commands/VerifySolution/VerifySolutionCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Algenic.Commons;
+using Algenic.Compilation;
+using Algenic.Compilation.Outputs;
+using Algenic.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Algenic.Commands.VerifySolution
+{
+    public class VerifySolutionCommandHandler : ICommandHandler<VerifySolutionCommand>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IRemoteCompiler<JDoodleOutput, JDoodleError> _compiler;
+
+        public VerifySolutionCommandHandler(ApplicationDbContext dbContext,
+            IRemoteCompiler<JDoodleOutput, JDoodleError> compiler)
+        {
+            _compiler = compiler;
+        }
+
+        public async Task HandleAsync(VerifySolutionCommand command)
+        {
+            var solution = await _dbContext.Solutions.SingleAsync(s => s.Id == command.SolutionId);
+            var tests = solution.Task.Tests.ToList();
+        }
+    }
+}

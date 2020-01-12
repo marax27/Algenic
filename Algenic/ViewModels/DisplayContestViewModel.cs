35ba107864ea06652da312e230d509ba790ac7fd
SlawekKalandyk <slawek.kalandyk@gmail.com>
2020-01-12 15:44:51 +0100

    Create DisplayContestViewModel

---
+++ Algenic/ViewModels/DisplayContestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Algenic.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Algenic.Data.Models.Contest;
+
+namespace Algenic.ViewModels
+{
+    public class DisplayContestViewModel
+    {
+        public string Name { get; set; }
+        public string OwnerId { get; set; }
+        public ContestState Status { get; set; }
+        public bool NotStarted { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
+    }
+}

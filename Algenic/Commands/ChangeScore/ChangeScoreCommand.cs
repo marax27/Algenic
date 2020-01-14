881077ecd541a18e4610af05637452c2105321b1
SlawekKalandyk <slawek.kalandyk@gmail.com>
2020-01-14 17:18:41 +0100

    Add ChangeScoreCommand to modify PointCount of existing solution

---
+++ Algenic/Commands/ChangeScore/ChangeScoreCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algenic.Commands.ChangeScore
+{
+    public class ChangeScoreCommand
+    {
+        public int SolutionId { get; }
+        public int Score { get; }
+
+        private ChangeScoreCommand(int solutionId, int score)
+        {
+            SolutionId = solutionId;
+            Score = score;
+        }
+
+        public static ChangeScoreCommand Create(int solutionId, int score)
+        {
+            return new ChangeScoreCommand(solutionId, score);
+        }
+    }
+}
---
+++ Algenic/Commands/ChangeScore/ChangeScoreCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Algenic.Commons;
+using Algenic.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algenic.Commands.ChangeScore
+{
+    public class ChangeScoreCommandHandler : ICommandHandler<ChangeScoreCommand>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ChangeScoreCommandHandler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task HandleAsync(ChangeScoreCommand command)
+        {
+            var solution = await _dbContext.Solutions.SingleAsync(s => s.Id == command.SolutionId);
+            solution.PointCount = command.Score;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}

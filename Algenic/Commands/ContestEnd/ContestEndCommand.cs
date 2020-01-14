0ac939bac4a1d7de6cb83760691aba4210cef42c
SlawekKalandyk <slawek.kalandyk@gmail.com>
2020-01-14 17:21:15 +0100

    Add ContestEndCommand to execute when Contest is completed

---
+++ Algenic/Commands/ContestEnd/ContestEndCommand.cs
@@ -0,0 +1,24 @@
+﻿using Algenic.Commons.DesignByContract;
+using Algenic.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algenic.Commands.ContestEnd
+{
+    public class ContestEndCommand
+    {
+        public int ContestId { get; }
+
+        private ContestEndCommand(int contestId)
+        {
+            ContestId = contestId;
+        }
+
+        public static ContestEndCommand Create(int contestId)
+        {
+            return new ContestEndCommand(contestId);
+        }
+    }
+}

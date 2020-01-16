f4ad9de0b2c514c81e40a1dbe536013ca364dce7
SlawekKalandyk <slawek.kalandyk@gmail.com>
2020-01-16 18:30:40 +0100

    Add UserResultsViewModel

---
+++ Algenic/ViewModels/UserResultsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Algenic.Queries.TaskScore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algenic.ViewModels
+{
+    public class UserResultsViewModel
+    {
+        public string Username { get; }
+        public int UserScore { get; }
+        public IEnumerable<TaskScoreQueryResult> TaskScores { get; }
+
+        public UserResultsViewModel(string username, IEnumerable<TaskScoreQueryResult> taskScores)
+        {
+            Username = username;
+            TaskScores = taskScores;
+            UserScore = taskScores
+                .Select(t => t.Score)
+                .Sum();
+        }
+    }
+}

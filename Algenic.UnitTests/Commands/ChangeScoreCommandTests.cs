72fee7c5d1a5d71cdf0d3fa4483bc4becb54bd22
unknown <mat_ski@op.pl>
2020-01-17 14:42:02 +0100

    Add ChangeScoreCommandTests

--- Algenic.UnitTests/Commands/ChangeScoreCommandTests.cs
+++ Algenic.UnitTests/Commands/ChangeScoreCommandTests.cs
@@ -1,10 +1,13 @@
 ﻿using Algenic.Data;
 using Algenic.UnitTests.Setup;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 using Algenic.Commands.ChangeScore;
+using Algenic.Commons;
+using Algenic.Data.Models;
+using Algenic.Compilation.Outputs;
+using Algenic.Compilation.Utilities;
+using Algenic.Queries.Compilation;
+using FluentAssertions;
 
 namespace Algenic.UnitTests.Commands
 {
@@ -15,7 +18,62 @@ namespace Algenic.UnitTests.Commands
         [Fact]
         public void Test1()
         {
+            var givenContest = new Contest
+            {
+                Name = "",
+                Status = Contest.ContestState.InProgress
+            };
+            Context.Contests.Add(givenContest);
+            var givenTask = new Task
+            {
+                Contest = givenContest,
+                Description = "",
+                Name = "",
+            };
+            Context.Tasks.Add(givenTask);
+            var givenTest = new Test
+            {
+                ExpectedOutput = "expected-output",
+                Input = "given-input",
+                Task = givenTask
+            };
+            Context.Tests.Add(givenTest);
+            var givenSolution = new Solution
+            {
+                SourceCode = "given-source-code",
+                Language = "java",
+                Task = givenTask
+            };
+            Context.Solutions.Add(givenSolution);
+            Context.SaveChanges();
 
+            var givenApiOutput = new JDoodleOutput
+            {
+                CpuTime = "1",
+                Memory = "2",
+                Output = "expected-output",
+                StatusCode = "200"
+            };
+            var givenQuery = CompilationQuery.Create("given-source-code", "given-input", ProgrammingLanguage.Java());
+            var givenResult = new CompilationQueryResult
+            {
+                ExecutionSuccessful = true,
+                Output = givenApiOutput
+            };
+
+            IQueryHandler<CompilationQuery, CompilationQueryResult> compilationMock =
+                new CompilationMockQueryHandler()
+                    .On(givenQuery).Returns(givenResult);
+
+            ICommandHandler<ChangeScoreCommand> sut =
+                new ChangeScoreCommandHandler(Context);
+
+            int newScore = 2;
+            var command = ChangeScoreCommand.Create(givenSolution.Id,newScore);
+
+            sut.HandleAsync(command).Wait();
+            var solution = Context.Solutions.Find(givenSolution.Id);
+            solution.PointCount.Should().Be(newScore);
         }
     }
 }

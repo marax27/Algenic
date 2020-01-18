898b4cb3beb6e6d5d9b0e9f9d8da1539e57014a2
SlawekKalandyk <slawek.kalandyk@gmail.com>
2020-01-18 12:20:43 +0100

    Fix file uploading

---
+++ Algenic/ViewModels/SolveTaskViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Algenic.Data.Models.Contest;
+
+namespace Algenic.ViewModels
+{
+    public class SolveTaskViewModel
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string ContestOwnerId { get; set; }
+        public ContestState ContestStatus { get; set; }
+    }
+}
